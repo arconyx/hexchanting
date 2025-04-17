12fbe8653571e4a7951968586847a2c3b1ed4be6
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-04-17 12:44:16 +1200

    don't double count media in simulation

--- src/main/kotlin/gay/thehivemind/hexchanting/casting/PackagedToolCastEnv.kt
+++ src/main/kotlin/gay/thehivemind/hexchanting/casting/PackagedToolCastEnv.kt
@@ -15,12 +15,25 @@ class PackagedToolCastEnv(caster: ServerPlayerEntity?, castingHand: Hand?, val t
         val casterHexHolder = IXplatAbstractions.INSTANCE.findHexHolder(tool) ?: return cost
         val canCastFromInv = casterHexHolder.canDrawMediaFromInventory()
         val casterMediaHolder = IXplatAbstractions.INSTANCE.findMediaHolder(tool)
-
         var costLeft = cost
+
+        // Calling extractMediaFromInventory twice may allow Oneironaut's BottomlessMediaItem to be drawn from
+        // twice per tick, but I don't think that is going to be a big deal.
+
+        // We want to draw media in the order
+        // Inventory
+        // Item (durability)
+        // Player (overcasting)
+        // To avoid double counting media in the inventory we need special simulation handling.
+
         // Start by drawing from the inventory, without overcasting
-        if (canCastFromInv && costLeft > 0) {
-            costLeft = this.extractMediaFromInventory(costLeft, false, simulate)
+        // Simulation is disabled here because we'll calculate it later
+        // Hopefully we won't have any problems with difference between sim and reality due
+        // to rounding of media amounts to item sizes
+        if (canCastFromInv && costLeft > 0 && !simulate) {
+            costLeft = this.extractMediaFromInventory(costLeft, false, false)
         }
+
         // Then draw from the item
         if (casterMediaHolder != null && costLeft > 0) {
             // The contracts on the AD and on this function are different.
@@ -28,10 +41,15 @@ class PackagedToolCastEnv(caster: ServerPlayerEntity?, castingHand: Hand?, val t
             val extracted = casterMediaHolder.withdrawMedia(costLeft, simulate)
             costLeft -= extracted
         }
+
         // Then overcast, if possible
-        if (canCastFromInv && costLeft > 0 && this.canOvercast()) {
-            costLeft = this.extractMediaFromInventory(costLeft, true, simulate)
+        // This is also the simulation run for inventory + overcasting
+        // This will cause an unnecessary second pass over the inventory for unenlightened casters
+        // But it's the cleanest way to fix the inventory double counting during simulation.
+        if (canCastFromInv && costLeft > 0) {
+            costLeft = this.extractMediaFromInventory(costLeft, this.canOvercast(), true)
         }
+
         return costLeft
     }
 }
