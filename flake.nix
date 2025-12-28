0940b9ec7f2651a735ff1d6e4e5a8835210c7ab7
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2025-12-29 12:08:44 +1300

    Remove flake-utils dependency

--- flake.nix
+++ flake.nix
@@ -1,27 +1,32 @@
 {
-  inputs = {
-    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
-  };
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+
   outputs =
-    { nixpkgs, flake-utils, ... }:
-    flake-utils.lib.eachDefaultSystem (
-      system:
-      let
-        pkgs = import nixpkgs { inherit system; };
-      in
-      {
-        devShells.default = pkgs.mkShell {
-          nativeBuildInputs = with pkgs; [
-            stdenv.cc.cc.lib
-            jdk21
-            libglvnd
-            xorg.libX11
-            python312
-            nodejs_20
-          ];
-          LD_LIBRARY_PATH = ''${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libglvnd}/lib:${pkgs.xorg.libX11}/lib'';
-        };
-      }
-    );
+    { nixpkgs, ... }:
+    let
+      # being a bit elaborate by passing pkgs as part of forAllSystems
+      # instead of just generating it as needed
+      packagesForSystem = system: nixpkgs.legacyPackages.${system};
+      # f is a function system (str) -> packages (attrset) -> attrset
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs nixpkgs.lib.systems.flakeExposed (system: f system (packagesForSystem system));
+    in
+    {
+      devShells = forAllSystems (
+        system: pkgs: {
+          default = pkgs.mkShell {
+            nativeBuildInputs = with pkgs; [
+              stdenv.cc.cc.lib
+              jdk21
+              libglvnd
+              xorg.libX11
+              python312
+              nodejs_20
+            ];
+            LD_LIBRARY_PATH = ''${pkgs.stdenv.cc.cc.lib}/lib:${pkgs.libglvnd}/lib:${pkgs.xorg.libX11}/lib'';
+          };
+        }
+      );
+    };
 }
