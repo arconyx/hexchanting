bc788c688ac8267b6f06a7a82324c779b4950cfd
ArcOnyx <11323309+arconyx@users.noreply.github.com>
2026-06-07 15:21:40 +1200

    Define all patterns with a dedicated property
    Again, this allows accessing them through other code.

--- src/main/kotlin/gay/thehivemind/hexchanting/casting/HexchantingPatterns.kt
+++ src/main/kotlin/gay/thehivemind/hexchanting/casting/HexchantingPatterns.kt
@@ -11,19 +11,15 @@ import net.minecraft.registry.Registry
 import net.minecraft.util.Identifier
 
 object HexchantingPatterns {
-    @JvmStatic
-    fun init() {
-        register("imbue_equipment", "dqaqdqaqdqaeadawadadawadadawa", HexDir.WEST, OpImbueEquipment())
+    val HEX_IMBUE_EQUIPMENT = register("imbue_equipment", "dqaqdqaqdqaeadawadadawadadawa", HexDir.WEST, OpImbueEquipment())
 
-    }
-
-    private fun register(name: String, signature: String, startDir: HexDir, action: Action) {
+    private fun register(name: String, signature: String, startDir: HexDir, action: Action): HexPattern {
+        val pattern = HexPattern.fromAngles(signature, startDir)
         Registry.register(
             HexActions.REGISTRY,
             Identifier.of(MOD_ID, name),
-            ActionRegistryEntry(HexPattern.fromAngles(signature, startDir), action)
+            ActionRegistryEntry(pattern, action)
         )
+        return pattern
     }
-
-
 }
